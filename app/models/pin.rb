a831d6876b7e76ac8237def00306f5d3dcca6a97
elmathud007 <elmathud007@gmail.com>
2016-01-26 11:37:42 +0000

    uploaded

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,17 +2,13 @@ class Pin < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>" },
-					:download,
-                    :storage => :s3,
-                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+	has_attached_file :image, :styles => { :medium => "300x300>" }
+					
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   
 
-  def s3_credentials
-    {:bucket => "xxx", :access_key_id => "xxx", :secret_access_key => "xxx"}
-  end
-end
+  
+
 end
